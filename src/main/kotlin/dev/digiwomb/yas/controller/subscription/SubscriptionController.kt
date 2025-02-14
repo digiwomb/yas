63e93df2ce792a4e2c15d217a1443c8785a36924
digiwomb <digiwomb@outlook.de>
2025-02-14 17:30:01 +0100

    Add get subscription and create sebcription endpoints

--- src/main/kotlin/dev/digiwomb/yas/controller/subscription/SubscriptionController.kt
+++ src/main/kotlin/dev/digiwomb/yas/controller/subscription/SubscriptionController.kt
@@ -1,15 +1,17 @@
 package dev.digiwomb.yas.controller.subscription
 
+import dev.digiwomb.yas.exception.SubscriptionNotFoundException
 import dev.digiwomb.yas.model.Subscription
 import dev.digiwomb.yas.repository.SubscriptionRepository
 import dev.digiwomb.yas.repository.UsersRepository
+import jakarta.validation.Valid
+import liquibase.statement.core.CreateTableStatement
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/subscription")
@@ -26,5 +28,28 @@ class SubscriptionController() {
         val subscriptions = subscriptionRepository.findByUser(user)
 
         return ResponseEntity.ok(subscriptions)
+    }
+
+    @GetMapping("/{id}")
+    fun getSubscriptionById(@PathVariable id: UUID, @AuthenticationPrincipal userDetails: UserDetails): ResponseEntity<Subscription> {
+        val user = usersRepository.findByEmail(userDetails.username).orElseThrow()
+        val subscription = subscriptionRepository.findById(id)
+            .filter { it.user == user }
+            .orElseThrow { SubscriptionNotFoundException() }
+        return ResponseEntity.ok(subscription)
+    }
+
+    @PostMapping("")
+    fun createSubscription(
+        @Valid @RequestBody request: CreateSubscriptionRequest,
+        @AuthenticationPrincipal userDetails: UserDetails
+    ): ResponseEntity<Subscription> {
+        val user = usersRepository.findByEmail(userDetails.username).orElseThrow()
+        val newSubscription = Subscription(
+            title = request.title,
+            amount = request.amount,
+            user = user
+        )
+        return ResponseEntity.ok(subscriptionRepository.save(newSubscription))
     }
 }
