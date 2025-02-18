852e27c9b6391f2a5b17bdd144fedeb7e3c99e69
digiwomb <digiwomb@outlook.de>
2025-02-18 23:06:24 +0100

    Add generic owned entity repository

---
+++ src/main/kotlin/dev/digiwomb/yas/helper/OwnedEntityRepository.kt
@@ -0,0 +1,12 @@
+package dev.digiwomb.yas.helper
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.NoRepositoryBean
+
+@NoRepositoryBean
+interface OwnedEntityRepository<T : OwnedEntity<ID, OWNER>, ID, OWNER> : JpaRepository<T, ID> {
+
+    fun findByOwner(owner: OWNER): List<T>
+
+    fun findByOwnerId(ownerID: ID): List<T>
+}
